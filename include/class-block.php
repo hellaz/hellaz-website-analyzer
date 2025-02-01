ae9e8dd451ae46e41002c86acc22a21c7a686c01
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:18:43 +0200

    Update class-block.php

--- include/class-block.php
+++ include/class-block.php
@@ -1,1 +1,24 @@
-# Gutenberg block
+// Gutenberg block
+<?php
+namespace Hellaz;
+
+class Block {
+    public static function init() {
+        add_action('init', [self::class, 'register_block']);
+    }
+
+    public static function register_block() {
+        register_block_type(HELLAZ_PLUGIN_PATH . 'blocks/analyzer', [
+            'render_callback' => [self::class, 'render_block']
+        ]);
+    }
+
+    public static function render_block($attributes) {
+        if (empty($attributes['url'])) return '';
+
+        ob_start();
+        $analysis_data = \Hellaz\Analysis\Engine::analyze(esc_url($attributes['url']));
+        include HELLAZ_PLUGIN_PATH . 'templates/block-output.php';
+        return ob_get_clean();
+    }
+}
