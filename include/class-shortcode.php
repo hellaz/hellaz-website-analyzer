6cd3e5aab823a4d0336d03c35eba68bab6948ff3
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:19:27 +0200

    Update class-shortcode.php

--- include/class-shortcode.php
+++ include/class-shortcode.php
@@ -1,1 +1,23 @@
-# Shortcode handler
+// Shortcode handler
+<?php
+namespace Hellaz;
+
+class Shortcode {
+    public static function init() {
+        add_shortcode('hellaz_analyzer', [self::class, 'handle_shortcode']);
+    }
+
+    public static function handle_shortcode($atts) {
+        $atts = shortcode_atts([
+            'url' => '',
+            'show' => 'all'
+        ], $atts);
+
+        if (empty($atts['url'])) return '';
+
+        ob_start();
+        $analysis_data = \Hellaz\Analysis\Engine::analyze(esc_url($atts['url']));
+        include HELLAZ_PLUGIN_PATH . 'templates/shortcode-output.php';
+        return ob_get_clean();
+    }
+}
