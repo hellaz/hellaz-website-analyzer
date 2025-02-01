b33cf11314be8923a93f3d755197f6867050fc8f
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:17:16 +0200

    Update class-analysis-exception.php

--- include/Exceptions/class-analysis-exception.php
+++ include/Exceptions/class-analysis-exception.php
@@ -1,1 +1,15 @@
+<?php
+namespace Hellaz\Exceptions;
 
+class AnalysisException extends \Exception {
+    protected $context = [];
+
+    public function __construct($message = "", $code = 0, $context = [], \Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+        $this->context = $context;
+    }
+
+    public function getContext() {
+        return $this->context;
+    }
+}
