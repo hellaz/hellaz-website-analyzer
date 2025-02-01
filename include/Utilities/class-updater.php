fa98225d2cd0539c910e26d500c159a6619f7e92
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:12:39 +0200

    Update class-updater.php

--- include/Utilities/class-updater.php
+++ include/Utilities/class-updater.php
@@ -1,1 +1,45 @@
+<?php
+namespace Hellaz\Utilities;
 
+class Updater {
+    private $file;
+    private $repo;
+
+    public function __construct($file) {
+        $this->file = $file;
+        $this->repo = 'https://github.com/hellaz/hellaz-website-analyzer';
+        add_filter('plugins_api', [$this, 'plugin_info'], 20, 3);
+        add_filter('site_transient_update_plugins', [$this, 'push_update']);
+    }
+
+    public function plugin_info($res, $action, $args) {
+        if ('plugin_information' !== $action) return $res;
+        if ($args->slug !== basename($this->file, '.php')) return $res;
+
+        $info = $this->get_remote_info();
+        return $info ? (object)$info : $res;
+    }
+
+    public function push_update($transient) {
+        if (empty($transient->checked)) return $transient;
+
+        $remote = $this->get_remote_info();
+        if ($remote && version_compare(HELLAZ_PLUGIN_VERSION, $remote['version'], '<')) {
+            $res = (object)[
+                'slug' => basename($this->file, '.php'),
+                'plugin' => plugin_basename($this->file),
+                'new_version' => $remote['version'],
+                'package' => $remote['download_url'],
+            ];
+            $transient->response[$res->plugin] = $res;
+        }
+        return $transient;
+    }
+
+    private function get_remote_info() {
+        $response = wp_remote_get($this->repo . '/releases/latest');
+        // Parse GitHub response and return version info
+        // Implementation depends on your release structure
+        return false; // Placeholder
+    }
+}
