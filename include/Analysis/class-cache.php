485455defcdc03b06b34a9e78f3e32edab1e9c0b
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:13:46 +0200

    Update class-cache.php

--- include/Analysis/class-cache.php
+++ include/Analysis/class-cache.php
@@ -1,1 +1,36 @@
+<?php
+namespace Hellaz\Analysis;
 
+class Cache {
+    public function get($url) {
+        $key = $this->get_cache_key($url);
+        $data = get_transient($key);
+        
+        if ($data && $this->is_cache_valid($data)) {
+            return $data['results'];
+        }
+        return false;
+    }
+
+    public function set($url, $data) {
+        $key = $this->get_cache_key($url);
+        $cache_data = [
+            'results' => $data,
+            'cached_at' => current_time('timestamp')
+        ];
+        set_transient($key, $cache_data, $this->get_ttl());
+    }
+
+    private function get_cache_key($url) {
+        return 'hellaz_analysis_' . md5($url);
+    }
+
+    private function get_ttl() {
+        return (int) get_option('hellaz_settings')['cache_ttl'] * HOUR_IN_SECONDS;
+    }
+
+    private function is_cache_valid($data) {
+        $max_age = $this->get_ttl();
+        return (time() - $data['cached_at']) < $max_age;
+    }
+}
