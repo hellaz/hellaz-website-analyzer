778154abeeb583e7aec4d91b766b5defe5326855
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-01 23:36:54 +0200

    Create class-cache.php

---
+++ include/class-cache.php
@@ -0,0 +1,11 @@
+// In class-cache.php
+namespace Hellaz;
+class Cache {
+    public static function get($key) {
+        return get_transient($key);
+    }
+
+    public static function set($key, $data, $expiration = 3600) {
+        set_transient($key, $data, $expiration);
+    }
+}
