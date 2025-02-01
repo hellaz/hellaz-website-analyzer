50c30363fd1b7836a501321cf697880436b5eceb
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 00:17:36 +0200

    Update class-api-exception.php

--- include/Exceptions/class-api-exception.php
+++ include/Exceptions/class-api-exception.php
@@ -1,1 +1,15 @@
+<?php
+namespace Hellaz\Exceptions;
 
+class ApiException extends \Exception {
+    protected $statusCode;
+
+    public function __construct($message = "", $statusCode = 500, \Throwable $previous = null) {
+        parent::__construct($message, 0, $previous);
+        $this->statusCode = $statusCode;
+    }
+
+    public function getStatusCode() {
+        return $this->statusCode;
+    }
+}
