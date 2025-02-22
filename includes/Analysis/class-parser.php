22c1c982e3968f84898386f3c4ca9422ca9e8423
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-22 14:06:53 +0200

    Update class-parser.php

--- includes/Analysis/class-parser.php
+++ includes/Analysis/class-parser.php
@@ -1,60 +1,62 @@
 <?php
 namespace Hellaz\Analysis;
 
-use Hellaz\Exceptions\AnalysisException;
-use Hellaz\Utilities\Formatter;
-
 class Parser {
-    public function parse($html, $url) {
+    public function parse($html_content, $url) {
         libxml_use_internal_errors(true);
         $dom = new \DOMDocument();
-        $dom->loadHTML($html);
-        $xpath = new \DOMXPath($dom);
+        @$dom->loadHTML($html_content);
 
-        return [
-            'metadata' => $this->extract_metadata($xpath),
-            'social' => $this->extract_social($xpath),
-            'feeds' => $this->find_feeds($html),
-            'security' => $this->analyze_security($url)
+        $result = [
+            'metadata' => $this->extract_metadata($dom),
+            'social_links' => $this->extract_social_links($dom),
+            'feeds' => $this->detect_feeds($dom),
+            'security' => $this->analyze_security($url),
         ];
+
+        libxml_clear_errors();
+        return $result;
     }
 
-    private function extract_metadata($xpath) {
+    private function extract_metadata($dom) {
         $metadata = [];
-        // Extract standard meta tags
-        $metadata['title'] = $this->get_first_match([
-            '//meta[@property="og:title"]/@content',
-            '//meta[@name="twitter:title"]/@content',
-            '//title/text()'
-        ], $xpath);
-
-        // Similar extraction for description, keywords, etc.
+        $meta_tags = $dom->getElementsByTagName('meta');
+        foreach ($meta_tags as $meta) {
+            $name = $meta->getAttribute('name') ?: $meta->getAttribute('property');
+            $content = $meta->getAttribute('content');
+            if ($name && $content) {
+                $metadata[$name] = $content;
+            }
+        }
         return $metadata;
     }
 
-    private function extract_social($xpath) {
-        $social = [];
-        // Find social links
-        $social_links = $xpath->query('//a[contains(@href, "twitter.com") or contains(@href, "facebook.com")]');
-        foreach ($social_links as $link) {
-            $social[] = [
-                'url' => $link->getAttribute('href'),
-                'platform' => $this->detect_platform($link->getAttribute('href'))
-            ];
+    private function extract_social_links($dom) {
+        $social_links = [];
+        $links = $dom->getElementsByTagName('a');
+        foreach ($links as $link) {
+            $href = $link->getAttribute('href');
+            if (strpos($href, 'facebook.com') !== false || strpos($href, 'twitter.com') !== false) {
+                $social_links[] = $href;
+            }
         }
-        return $social;
+        return $social_links;
     }
 
-    private function find_feeds($html) {
-        preg_match_all('/<link[^>]+(type="application\/(rss|atom)\+xml")[^>]+href="([^"]+)"/i', $html, $matches);
-        return $matches[3] ?? [];
+    private function detect_feeds($dom) {
+        $feeds = [];
+        $links = $dom->getElementsByTagName('link');
+        foreach ($links as $link) {
+            if ($link->getAttribute('rel') === 'alternate' && in_array($link->getAttribute('type'), ['application/rss+xml', 'application/atom+xml'])) {
+                $feeds[] = $link->getAttribute('href');
+            }
+        }
+        return $feeds;
     }
 
     private function analyze_security($url) {
-        $domain = Formatter::normalize_domain($url);
-        return [
-            'https' => parse_url($url, PHP_URL_SCHEME) === 'https',
-            'hsts' => $this->check_hsts($domain)
-        ];
+        $security_info = [];
+        // Placeholder for security analysis logic
+        return $security_info;
     }
 }
